ac5bd9f12370b1e46d0e38890cf36aff93292eb0
KMDzik <katarzyna.dzik90@gmail.com>
2020-03-17 22:29:11 +0100

    The first order

--- src/Library.java
+++ src/Library.java
@@ -1,5 +1,18 @@
 public class Library {
     public static void main(String[] args) {
 
+        final String appName = "Library v0.1";
+
+        String title = "Automating and Testing";
+        String author = "A.Richardson";
+        int year = 2017;
+        String cartNumber = "1234567";
+
+        System.out.println(appName);
+        System.out.println("Book ordered:");
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(year);
+        System.out.println(cartNumber);
     }
 }
